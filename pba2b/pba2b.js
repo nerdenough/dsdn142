77ad973bf27f705e5d26aafd3ea0b9f1470e2051
Brendan Goodenough <brendan@goodenough.nz>
2017-01-11 20:06:53 +1300

    Add base pba2b files

---
+++ pba2b/pba2b.js
@@ -0,0 +1,20 @@
+/**
+ * Brendan Goodenough
+ * PBA2b
+ *
+ * Example 4-12: Pins and Lines
+ * from Getting Started with Processing by Casey Reas and Ben Fry
+ */
+
+function setup() {
+  // https://github.com/processing/p5.js/wiki/Beyond-the-canvas
+  // Injects the canvas into a specific container
+  const canvas = createCanvas(600, 600);
+  canvas.parent('canvas');
+
+  background(255);
+  smooth();
+}
+
+function draw() {
+}

240ab326104818b327e0d27ac7319ae05019997c
Brendan Goodenough <brendan@goodenough.nz>
2017-01-15 10:13:26 +1300

    Add base formula-lines files

---
+++ p1/formula-lines/formula-lines.js
@@ -0,0 +1,18 @@
+/**
+ * Brendan Goodenough
+ * Project 1 - Formula Lines
+ *
+ * Based on the Project 1 "Lines Formula" template by Birgit Bachler
+ */
+
+function setup() {
+  // Injects the canvas into a specific container
+  const canvas = createCanvas(600, 600);
+  canvas.parent('canvas');
+  smooth();
+}
+
+function draw() {
+  clear();
+  background(255);
+}

7b0fc00880fdacbe0cc190638c2d009e01115159
Brendan Goodenough <brendan@goodenough.nz>
2017-01-15 10:10:38 +1300

    Draw curves between random points

--- p1/free-lines/free-lines.js
+++ p1/free-lines/free-lines.js
@@ -7,11 +7,49 @@
 
 const numPoints = 24;
 const points = [];
+const lines = [];
+
+function shortestPath() {
+  const path = [];
+  const visited = [];
+  let unvisited = points.slice(0);
+
+  points.map(p => {
+    let shortest;
+
+    unvisited.map(u => {
+      if (p !== u) {
+        if (!shortest) {
+          shortest = u;
+        }
+
+        const lenX = Math.abs(p.x - u.x);
+        const lenY = Math.abs(p.y - u.y);
+
+        if (lenX < shortest.x && lenY < shortest.y) {
+          shortest = u;
+        }
+      }
+    });
+
+    if (shortest) {
+      lines.push({
+        x1: p.x,
+        y1: p.y,
+        x2: shortest.x,
+        y2: shortest.y
+      });
+    }
+
+    const index = unvisited.indexOf(shortest);
+    unvisited.splice(index, 1);
+  });
+}
 
 function createPoints() {
   for (let i = 0; i < numPoints; i++) {
-    const x = Math.random() * width;
-    const y = Math.random() * height;
+    const x = Math.random() * (width - 100) + 50;
+    const y = Math.random() * (height - 100) + 50;
 
     points.push({ x, y });
   }
@@ -24,6 +62,7 @@ function setup() {
   smooth();
 
   createPoints();
+  shortestPath();
 }
 
 function draw() {
@@ -32,5 +71,8 @@ function draw() {
 
   // Draw base isometric grid
   stroke(0);
-  points.map(p => line(p.x, p.y, p.x, p.y));
+  strokeWeight(5);
+  beginShape();
+  points.map(p => curveVertex(p.x, p.y));
+  endShape();
 }

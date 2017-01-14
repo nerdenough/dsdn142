547d8e5015fe9278d636ee9978900779327dd6e5
Brendan Goodenough <brendan@goodenough.nz>
2017-01-14 20:19:47 +1300

    Significantly optimise drawing isometric lines
    Reduces a lot of repetetive code, and includes a function for drawing
    from one point in the grid to another.

--- p1/grid-lines/grid-lines.js
+++ p1/grid-lines/grid-lines.js
@@ -5,83 +5,39 @@
  * Based on the Project 1 "Lines Grid" template by Birgit Bachler
  */
 
-// Lines to be drawn along the horizontal and vertical axis of the isometric
-// grid.
-const xLines = [];
-const yLines = [];
-const tileSize = 64;
+const grid = [];
+const length = 10;
+const spacing = 64;
 
 /**
- * Calculates the horizontal line locations for for an isometric grid. The lines
- * are built upon an initial grid and a tile size to determine their location
- * on screen.
+ * Draws a line from one point to another.
  *
- * @param {Array} grid - The initial grid to build upon.
+ * @param {Object} p1 - Coordinates for first point.
+ * @param {Object} p2 - Coordiantes for second point.
  */
-function calculateXLines(grid) {
-  grid[0].map((point, index) => {
-    // http://clintbellanger.net/articles/isometric_math/
-    // Calculate location for lines on screen
-    const screenX = (point.x - point.y) * (tileSize / 2) + width / 2;
-    const screenY = (point.x + point.y) * (tileSize / 2) + height / 2;
-    const destX = grid[grid.length - 1][index].x;
-    const destY = grid[grid.length - 1][index].y;
-    const screenDestX = (destX - destY) * (tileSize / 2) + width / 2;
-    const screenDestY = (destX + destY) * (tileSize / 2) + height / 2;
+function drawIsometricLine(p1, p2) {
+  // http://clintbellanger.net/articles/isometric_math/
+  const x1 = (p1.x - p1.y) * (spacing / 2) + width / 2;
+  const y1 = (p1.x + p1.y) * (spacing / 2) + height / 2;
+  const x2 = (p2.x - p2.y) * (spacing / 2) + width / 2;
+  const y2 = (p2.x + p2.y) * (spacing / 2) + height / 2;
 
-    xLines.push({
-      start: { x: screenX, y: screenY / 2 },
-      end: { x: screenDestX, y: screenDestY / 2 }
-    });
-  });
-}
-
-/**
- * Calculates the vertical line locations for for an isometric grid. The lines
- * are built upon an initial grid and a tile size to determine their location
- * on screen.
- *
- * @param {Array} grid - The initial grid to build upon.
- */
-function calculateYLines(grid) {
-  grid.map((col, index) => {
-    const point = col[0];
-
-    // http://clintbellanger.net/articles/isometric_math/
-    // Calculate location for lines on screen
-    const screenX = (point.x - point.y) * (tileSize / 2) + width / 2;
-    const screenY = (point.x + point.y) * (tileSize / 2) + height / 2;
-    const destX = grid[index][grid.length - 1].x;
-    const destY = grid[index][index].y;
-    const screenDestX = (destX - destY) * (tileSize / 2) + width / 2;
-    const screenDestY = (destX + destY) * (tileSize / 2) + height / 2;
-
-    yLines.push({
-      start: { x: screenX, y: screenY / 2 },
-      end: { x: screenDestX, y: screenDestY / 2 }
-    });
-  });
+  line(x1, y1 / 2, x2, y2 / 2);
 }
 
 /**
  * Constructs an initial grid based on the canvas size and specified tile size.
  * The grid, stored as a 2D array, contains objects which store the x and y
  * indexes of each of the points.
- *
- * @returns {Array} Initial grid.
  */
 function createGrid() {
-  const grid = [];
-
-  for (let y = 0; y <= height / tileSize; y++) {
+  for (let y = 0; y < length; y++) {
     const cols = [];
-    for (let x = 0; x <= width / tileSize; x++) {
+    for (let x = 0; x < length; x++) {
       cols.push({ x, y });
     }
     grid.push(cols);
   }
-
-  return grid;
 }
 
 function setup() {
@@ -91,8 +47,6 @@ function setup() {
   smooth();
 
   const grid = createGrid();
-  calculateXLines(grid);
-  calculateYLines(grid);
 }
 
 function draw() {
@@ -101,6 +55,6 @@ function draw() {
 
   // Draw base isometric grid
   stroke('#97cff2');
-  xLines.map(l => line(l.start.x, l.start.y, l.end.x, l.end.y));
-  yLines.map(l => line(l.start.x, l.start.y, l.end.x, l.end.y));
+  grid[0].map((p, index) => drawIsometricLine(p, grid[length - 1][index]));
+  grid.map((col, index) => drawIsometricLine(col[0], grid[index][length - 1]));
 }

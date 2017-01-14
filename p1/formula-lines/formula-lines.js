bfd07dd5e797352acc9624d17e32f26f37c3ae79
Brendan Goodenough <brendan@goodenough.nz>
2017-01-15 12:04:19 +1300

    Draw 2 sets of sketch waves

--- p1/formula-lines/formula-lines.js
+++ p1/formula-lines/formula-lines.js
@@ -7,37 +7,43 @@
  */
 
 const xspacing = 8;
-const amplitude = 150.0;
 const period = 600.0;
+const waves = [];
+const waveCount = 4;
 let theta = 0.0;
-let dx;
 let w;
-let yvalues;
+let length;
 
 /**
  * Calculate the y-values for the wave, incrementing it's initial x-value
  * through each call to create a moving sine wave.
  */
-function calcWave() {
-  theta += 0.02;
+function calcWaves() {
+  const amplitude = Math.random() * (160.0 - 150.0) + 150.0;
+  const yvalues = [];
 
+  theta += 0.01;
   let x = theta;
-  for (let i = 0; i < yvalues.length; i++) {
-    yvalues[i] = sin(x) * amplitude;
+
+  for (let i = 0; i < length; i++) {
+    yvalues.push(sin(x) * amplitude);
     x += dx;
   }
+  waves.push(yvalues);
 }
 
 /**
  * Draw the wave to the screen, using the spacing to calculate the x-value,
  * and the current index in the y-values for the y-value.
  */
-function renderWave() {
-  beginShape();
-  yvalues.map((val, i) => {
-    vertex(i * xspacing, height / 2 + yvalues[i]);
+function renderWaves(yOffset) {
+  waves.map(yvalues => {
+    beginShape();
+    yvalues.map((val, i) => {
+      vertex(i * xspacing, height / 2 + val + yOffset);
+    });
+    endShape();
   });
-  endShape();
 }
 
 function setup() {
@@ -49,7 +55,11 @@ function setup() {
   // Calculate spacing and velocity
   w = width + xspacing;
   dx = (TWO_PI / period) * xspacing;
-  yvalues = new Array(floor(w / xspacing));
+  length = floor(w / xspacing);
+
+  for (let i = 0; i < waveCount; i++) {
+    calcWaves();
+  }
 }
 
 function draw() {
@@ -59,6 +69,6 @@ function draw() {
   noFill();
 
   // Calculate and render the wave
-  calcWave();
-  renderWave();
+  renderWaves(-20);
+  renderWaves(20);
 }

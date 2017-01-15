a525c5ec7ba95e68f3f98239138da515e20c2e19
Brendan Goodenough <brendan@goodenough.nz>
2017-01-15 14:46:12 +1300

    Add sketch effect to grid-lines

--- p1/grid-lines/grid-lines.js
+++ p1/grid-lines/grid-lines.js
@@ -6,23 +6,41 @@
  */
 
 const grid = [];
+const lines = [];
 const length = 10;
 const spacing = 64;
 
 /**
- * Draws a line from one point to another.
- *
- * @param {Object} p1 - Coordinates for first point.
- * @param {Object} p2 - Coordiantes for second point.
+ * Convert normal grid point to isometric.
  */
-function drawIsometricLine(p1, p2) {
+function calcPoint(p) {
   // http://clintbellanger.net/articles/isometric_math/
-  const x1 = (p1.x - p1.y) * (spacing / 2) + width / 2;
-  const y1 = (p1.x + p1.y) * (spacing / 2) + height / 2;
-  const x2 = (p2.x - p2.y) * (spacing / 2) + width / 2;
-  const y2 = (p2.x + p2.y) * (spacing / 2) + height / 2;
+  const isoX = (p.x - p.y) * (spacing / 2) + width / 2;
+  const isoY = ((p.x + p.y) * (spacing / 2) + height / 2) / 2 + 6;
 
-  line(x1, y1 / 2, x2, y2 / 2);
+  return { x: isoX, y: isoY };
+}
+
+/**
+ * Calculate grid points by converting the coordinates to isometric view and
+ * creating lines based on random variations of these points.
+ */
+function calcGridPoints(dx, dy) {
+  for (let i = 0; i < grid.length - 1; i++) {
+    for (let j = 0; j < grid[0].length - 1; j++) {
+      // Calculate isometric coordinates
+      const p1 = calcPoint(grid[i][j]);
+      const p2 = calcPoint(grid[i][j + 1]);
+      const p3 = calcPoint(grid[i + 1][j]);
+
+      // Generate sketch lines
+      for (let k = 0; k < 5; k++) {
+        const dx = Math.random() * 5 - 2.5;
+        const dy = Math.random() * 5 - 2.5;
+        lines.push({ p1, p2, p3, dx, dy });
+      }
+    }
+  }
 }
 
 /**
@@ -47,14 +65,28 @@ function setup() {
   smooth();
 
   const grid = createGrid();
+  calcGridPoints();
 }
 
 function draw() {
   clear();
   background(255);
 
-  // Draw base isometric grid
-  stroke('#97cff2');
-  grid[0].map((p, index) => drawIsometricLine(p, grid[length - 1][index]));
-  grid.map((col, index) => drawIsometricLine(col[0], grid[index][length - 1]));
+  // Iterate through each line in the lines array
+  lines.map((l, i) => {
+    const p1 = l.p1;
+    const p2 = l.p2;
+    const p3 = l.p3;
+    const dx = l.dx;
+    const dy = l.dy;
+
+    // Alternate stroke colour
+    i % 2 === 0
+      ? stroke(116, 187, 232, 150)
+      : stroke(242, 116, 160, 100);
+
+    // Draw horizontal and vertical lines
+    line(p1.x + dx, p1.y + dy, p2.x + dx, p2.y + dy);
+    line(p1.x + dx, p1.y + dy, p3.x + dx, p3.y + dy);
+  });
 }

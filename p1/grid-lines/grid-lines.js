35a83b760a084e74ba3fda737a58f7c99cd6da2a
Brendan Goodenough <brendan@goodenough.nz>
2017-01-16 22:54:31 +1300

    Revamp grid-lines

--- p1/grid-lines/grid-lines.js
+++ p1/grid-lines/grid-lines.js
@@ -5,56 +5,26 @@
  * Based on the Project 1 "Lines Grid" template by Birgit Bachler
  */
 
-const grid = [];
-const lines = [];
-const length = 10;
-const spacing = 64;
-
 /**
- * Convert normal grid point to isometric.
+ * Draws the grid
  */
-function calcPoint(p) {
-  // http://clintbellanger.net/articles/isometric_math/
-  const isoX = (p.x - p.y) * (spacing / 2) + width / 2;
-  const isoY = ((p.x + p.y) * (spacing / 2) + height / 2) / 2 + 6;
+function drawGrid() {
+  for (let y = 0; y <= height; y += 50) {
+    for (let x = 0; x <= width; x += 50) {
+      // Draw the angles lines, getting darker with each x iteration
+      for (let i = 0; i <= x / 50; i++) {
+        line(x, y, x + 50, y + 50);
+        line(x, y, x - 50, y + 50);
+      }
 
-  return { x: isoX, y: isoY };
-}
-
-/**
- * Calculate grid points by converting the coordinates to isometric view and
- * creating lines based on random variations of these points.
- */
-function calcGridPoints(dx, dy) {
-  for (let i = 0; i < grid.length - 1; i++) {
-    for (let j = 0; j < grid[0].length - 1; j++) {
-      // Calculate isometric coordinates
-      const p1 = calcPoint(grid[i][j]);
-      const p2 = calcPoint(grid[i][j + 1]);
-      const p3 = calcPoint(grid[i + 1][j]);
-
-      // Generate sketch lines
-      for (let k = 0; k < 5; k++) {
-        const dx = Math.random() * 5 - 2.5;
-        const dy = Math.random() * 5 - 2.5;
-        lines.push({ p1, p2, p3, dx, dy });
+      // Draw the shading between lines
+      for (let i = 0; i < 25; i++) {
+        line(x, y, x + i, y + 25);
+        line(x, y, x - i, y + 25);
+        line(x, y, x + i, y - 25);
+        line(x, y, x - i, y - 25);
       }
     }
-  }
-}
-
-/**
- * Constructs an initial grid based on the canvas size and specified tile size.
- * The grid, stored as a 2D array, contains objects which store the x and y
- * indexes of each of the points.
- */
-function createGrid() {
-  for (let y = 0; y < length; y++) {
-    const cols = [];
-    for (let x = 0; x < length; x++) {
-      cols.push({ x, y });
-    }
-    grid.push(cols);
   }
 }
 
@@ -64,29 +34,14 @@ function setup() {
   canvas.parent('canvas');
   smooth();
 
-  const grid = createGrid();
-  calcGridPoints();
 }
 
 function draw() {
   clear();
   background(255);
+  stroke(0, 0, 0, 20);
 
-  // Iterate through each line in the lines array
-  lines.map((l, i) => {
-    const p1 = l.p1;
-    const p2 = l.p2;
-    const p3 = l.p3;
-    const dx = l.dx;
-    const dy = l.dy;
-
-    // Alternate stroke colour
-    i % 2 === 0
-      ? stroke(116, 187, 232, 150)
-      : stroke(242, 116, 160, 100);
-
-    // Draw horizontal and vertical lines
-    line(p1.x + dx, p1.y + dy, p2.x + dx, p2.y + dy);
-    line(p1.x + dx, p1.y + dy, p3.x + dx, p3.y + dy);
-  });
+  // Translate start by 25 on the x-axis
+  translate(25, 0);
+  drawGrid();
 }

e776e7caea2893382cf4ce2755fa48fd72646a9c
Brendan Goodenough <brendan@goodenough.nz>
2017-01-17 00:33:08 +1300

    Tidy grid-polygons code

--- p1/grid-polygons/grid-polygons.js
+++ p1/grid-polygons/grid-polygons.js
@@ -5,8 +5,6 @@
  * Based on the Project 1 "Lines Grid" template by Birgit Bachler
  */
 
-let theta;
-
  /**
   * Creates a polygon of n points at a given location.
   * Based on https://p5js.org/examples/form-regular-polygon.html
@@ -15,12 +13,11 @@ let theta;
   * @param {Number} y - Location on the y-axis.
   * @param {Number} radius - Polygon radius.
   * @param {Number} npoints - Number of vertices.
-  * @param {Number} theta - Initial angle in radians.
   */
-function polygon(x, y, radius, npoints, theta) {
+function polygon(x, y, radius, npoints) {
  const angle = TWO_PI / npoints;
  beginShape();
- for (let a = theta; a < theta + TWO_PI; a += angle) {
+ for (let a = 0; a < TWO_PI; a += angle) {
    const sx = x + cos(a) * radius;
    const sy = y + sin(a) * radius;
    vertex(sx, sy);
@@ -34,14 +31,17 @@ function polygon(x, y, radius, npoints, theta) {
 function drawGrid() {
   for (let y = 0; y <= height; y += 50) {
     for (let x = 0; x <= width; x += 50) {
+      // Only shade hexagons if multiple of 3 on x or y axis
       if (y / 50 % 3 && x / 50 % 3) {
-        polygon(x, y, 20, 6, 0);
+        // Hexagon outline
+        polygon(x, y, 20, 6);
 
+        // Draw inner hexagons
         for (let i = 0; i < 10; i++) {
-          polygon(x, y, 20 - 2 * i, 6, 0);
+          polygon(x, y, 20 - 2 * i, 6);
         }
       } else if (x > 0 && y > 0 && x < width && y < height) {
-        polygon(x, y, 20, 6, 0);
+        polygon(x, y, 20, 6);
       }
     }
   }

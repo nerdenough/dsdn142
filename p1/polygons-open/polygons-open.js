36fa90876a8fa4ca501a4165f6919c8aecad1ccf
Brendan Goodenough <brendan@goodenough.nz>
2017-01-17 19:54:44 +1300

    Swap polygons-formula and polygons-grid

--- p1/polygons-open/polygons-open.js
+++ p1/polygons-open/polygons-open.js
@@ -1,27 +1,27 @@
 /**
  * Brendan Goodenough
- * Project 1 - Ellipses Open
+ * Project 1 - Polygons Open
  */
 
-/**
- * Creates a polygon of n points at a given location.
- * Based on https://p5js.org/examples/form-regular-polygon.html
- *
- * @param {Number} x - Location on the x-axis.
- * @param {Number} y - Location on the y-axis.
- * @param {Number} radius - Polygon radius.
- * @param {Number} npoints - Number of vertices.
- */
-function polygon(x, y, radius, npoints) {
-  const angle = TWO_PI / npoints;
-  beginShape();
-  for (let a = 0; a < TWO_PI; a += angle) {
-    const sx = x + cos(a) * radius;
-    const sy = y + sin(a) * radius;
-    vertex(sx, sy);
-  }
-  endShape(CLOSE);
-}
+ /**
+  * Creates a polygon of n points at a given location.
+  * Based on https://p5js.org/examples/form-regular-polygon.html
+  *
+  * @param {Number} x - Location on the x-axis.
+  * @param {Number} y - Location on the y-axis.
+  * @param {Number} radius - Polygon radius.
+  * @param {Number} npoints - Number of vertices.
+  */
+ function polygon(x, y, radius, npoints) {
+   const angle = TWO_PI / npoints;
+   beginShape();
+   for (let a = 0; a < TWO_PI; a += angle) {
+     const sx = x + cos(a) * radius;
+     const sy = y + sin(a) * radius;
+     vertex(sx, sy);
+   }
+   endShape(CLOSE);
+ }
 
 function setup() {
   // Injects the canvas into a specific container
@@ -40,11 +40,14 @@ function draw() {
   // https://p5js.org/reference/#/p5/translate
   translate(width / 2, height / 2);
 
-  // Draw 100 hexagons at different positions, rotating by 1 each time
   for (let i = 0; i < 100; i++) {
-    rotate(1);
-    polygon(0, 0, 80 + i, 6);
-    polygon(0, 0, 120 + i, 6);
-    polygon(0, 0, 160 + i, 6);
+    rotate(0);
+    polygon(i, i, 200, 3);
+    rotate(HALF_PI);
+    polygon(i, i, 200, 3);
+    rotate(PI);
+    polygon(i, i, 200, 3);
+    rotate(HALF_PI + PI);
+    polygon(i, i, 200, 3);
   }
 }

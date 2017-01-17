5d39210403ef94c16eb1b347deb601d927738270
Brendan Goodenough <brendan@goodenough.nz>
2017-01-17 19:55:58 +1300

    Darken lines-formula

--- p1/lines-formula/lines-formula.js
+++ p1/lines-formula/lines-formula.js
@@ -85,7 +85,7 @@ function draw() {
   clear();
   background(255);
   noFill();
-  stroke(0, 0, 0, 30);
+  stroke(0, 0, 0, 80);
 
   // Render the waves with their connecting lines
   for (let i = 0; i < 100; i++) {

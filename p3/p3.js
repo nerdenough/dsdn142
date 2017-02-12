d81e48700bff9c1479018f47ecbaad71018c8f8a
Brendan Goodenough <brendan@goodenough.nz>
2017-02-12 20:54:39 +1300

    Draw pendulum balls

--- p3/p3.js
+++ p3/p3.js
@@ -3,11 +3,46 @@
  * Project 3
  */
 
+let balls = [];
+
+function createBalls() {
+  const balls = [];
+
+  balls.push({
+    x: width / 2,
+    y: height - 200
+  });
+
+  balls.push({
+    x: width / 2 - 150,
+    y: height - 200
+  });
+
+  balls.push({
+    x: width / 2 - 300,
+    y: height - 200
+  });
+
+  balls.push({
+    x: width / 2 + 150,
+    y: height - 200
+  });
+
+  balls.push({
+    x: width / 2 + 300,
+    y: height - 200
+  });
+
+  return balls;
+}
+
 function setup() {
   // https://github.com/processing/p5.js/wiki/Beyond-the-canvas
   // Injects the canvas into a specific container
   const canvas = createCanvas(1280, 720);
   canvas.parent('canvas');
+
+  balls = createBalls();
 
   smooth();
 }
@@ -15,4 +50,10 @@ function setup() {
 function draw() {
   clear();
   background(255);
+
+  balls.map(ball => ellipse(ball.x, ball.y, 100, 100));
+}
+
+function mouseClicked() {
+  // TODO: Change ball colours
 }

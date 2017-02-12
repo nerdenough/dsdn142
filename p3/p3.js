5e7cd445cefc4db9368b793d9966a0a420d5077f
Brendan Goodenough <brendan@goodenough.nz>
2017-02-12 21:35:28 +1300

    Replace manual creation of pendulums with iteration

--- p3/p3.js
+++ p3/p3.js
@@ -1,39 +1,31 @@
 /**
  * Brendan Goodenough
- * Project 3
+ * Project 3 - The Cats in the Cradle
+ *
+ * I am attempting to complete this project using primarily Functional
+ * Programming, hence some syntax might seem a bit inefficient for
+ * creating basic structures.
+ *
+ * Ironically, Functional Programming revolves around the absence of side
+ * effects, yet a Newton's Cradle is a literal visualisation of side effects.
+ * In a weird way that seems rather fitting.
  */
 
-let balls = [];
+let pendulums = [];
 
-function createBalls() {
-  const balls = [];
-
-  balls.push({
-    x: width / 2,
+/**
+ * Initialises the cradle by creating the five pendulums.
+ *
+ * @returns {Array} The newly created pendulums.
+ */
+function createPendulums() {
+  // Iterates over a basic array, creating a new array with the correct x and y
+  // values. I'm gonna assume there's a better way to do this without using a
+  // for loop, but this does the trick.
+  return [1, 2, 3, 4, 5].map((item, i) => ({
+    x: 375 + (i * 150) - 30,
     y: height - 200
-  });
-
-  balls.push({
-    x: width / 2 - 150,
-    y: height - 200
-  });
-
-  balls.push({
-    x: width / 2 - 300,
-    y: height - 200
-  });
-
-  balls.push({
-    x: width / 2 + 150,
-    y: height - 200
-  });
-
-  balls.push({
-    x: width / 2 + 300,
-    y: height - 200
-  });
-
-  return balls;
+  }));
 }
 
 function setup() {
@@ -42,7 +34,7 @@ function setup() {
   const canvas = createCanvas(1280, 720);
   canvas.parent('canvas');
 
-  balls = createBalls();
+  pendulums = createPendulums();
 
   smooth();
 }
@@ -52,12 +44,12 @@ function draw() {
   background(255);
   strokeWeight(5);
 
-  balls.map((ball) => {
-    line(ball.x, 0, ball.x, ball.y);
-    ellipse(ball.x, ball.y, 150, 150);
+  pendulums.map((pendulum) => {
+    line(pendulum.x, 0, pendulum.x, pendulum.y);
+    ellipse(pendulum.x, pendulum.y, 150, 150);
   });
 }
 
 function mouseClicked() {
-  // TODO: Change ball colours
+  // TODO: Change pendulum colours
 }
